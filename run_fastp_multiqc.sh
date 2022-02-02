5467ccb7bd56a9db35bafd173f0e9a1d7bb511ce
Shweta Chavan <chavan.shweta@gmail.com>
2022-02-02 06:59:46 -0500

    Update and rename run_fastp.sh to run_fastp_multiqc.sh

--- run_fastp_multiqc.sh
+++ run_fastp_multiqc.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+$QCFOLDER="outputsQC"
+SAMPLESHEET1="../inputs/samplesheet_EXP21001376.tsv"
+
+###Edit above block to configure appropiate paths
+
+mkdir -p $QCFOLDER
 while read -r SAMPLE FASTQ1 FASTQ2 REST; do
     echo "Working on --> "$SAMPLE
     fastp -h outputsQC/${SAMPLE}_Shallow_fastp.html -j outputsQC/${SAMPLE}_Shallow_fastp.json -i ${FASTQ1} -I ${FASTQ2}
-done < ../inputs/samplesheet_EXP21001376.tsv
+done < $SAMPLESHEET1
+
+multiqc $QCFOLDER
