d655196bdd183f8a0f4af11cdad369f8a15515d5
Shweta Chavan <chavan.shweta@gmail.com>
2022-02-02 08:12:31 -0500

    Update mergelanes.sh
    Helper script to merge lanes

--- mergelanes.sh
+++ mergelanes.sh
@@ -1,3 +1,5 @@
+#Helper script to merge lanes when delimiter is the first '_', change accordingly for your files
+
 ls *R1* | cut -d _ -f 1 | sort | uniq \
     | while read id; do \
         cat $id*R1*.fastq.gz > $id.R1.fastq.gz;
