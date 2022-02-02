e72842188d8b2f54496800f45b42f74b3843619b
Shweta Chavan <chavan.shweta@gmail.com>
2022-02-02 07:37:13 -0500

    Create 5_run_vdjtools_custom_samples.sh

---
+++ 5_run_vdjtools_custom_samples.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#conda init bash
+#conda activate tcrseq
+metadataToConvert='/home/schavan/projects/bulk_tcr_seq/inputs/metadataToConvert_EXP21001376_FFPE.txt'
+metadata='metadata.txt'
+
+ 	sample_name1="HNSCC-15396-1"
+ 	sample_name2="HNSCC-15396-2"
+  sample_name3="HNSCC-15396-3"
+  output_prefix="HNSCC-15396"
+
+
+## Repertoire Overlap - replicates/pairs e.g. Comparing 3 samples with all permuations
+
+       vdjtools OverlapPair -p -t 15 VDJtools.${sample_name1}.txt VDJtools.${sample_name2}.txt ${output_prefix}
+       vdjtools OverlapPair -p -t 15 VDJtools.${sample_name2}.txt VDJtools.${sample_name3}.txt ${output_prefix}
+       vdjtools OverlapPair -p -t 15 VDJtools.${sample_name1}.txt VDJtools.${sample_name3}.txt ${output_prefix}
+
+       vdjtools JoinSamples -p VDJtools.${sample_name1}.txt VDJtools.${sample_name2}.txt VDJtools.${sample_name3}.txt ${output_prefix}
+    
+    
