c8b558f95b4bd8b7144ca2b5a4b8b8dfc392e412
Shweta Chavan <chavan.shweta@gmail.com>
2022-02-02 07:17:34 -0500

    Create 4_run_vdjtools_single_samples.sh

---
+++ 4_run_vdjtools_single_samples.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#conda init bash
+#conda activate tcrseq
+
+metadataToConvert='/home/schavan/projects/bulk_tcr_seq/inputs/metadataToConvert_EXP21001376_FFPE.txt'
+metadata='metadata.txt' #automatically created by vdjtools by below code
+
+while read -r file_name sample_name REST; do
+
+     echo "Working on --> "$counter $sample_name
+     #mkdir -p /home/schavan/projects/bulk_tcr_seq/outputs/vdjtools.out/${sample_name}
+
+ ## Basic Stats using metadata file
+     vdjtools Convert -S mixcr -m ${metadataToConvert} VDJtools
+     ##Step2 after metdata file is created
+     vdjtools CalcBasicStats -m ${metadata} ${sample_name}
+     vdjtools CalcSegmentUsage -p -m ${metadata} ${sample_name}
+     vdjtools CalcSpectratype -m ${metadata} ${sample_name}
+
+ ## Basic plots without using metadata file
+     vdjtools PlotFancySpectratype VDJtools.${sample_name}.txt ${sample_name}
+     ./vdjtools-patch.sh vdjtools PlotFancyVJUsage VDJtools.${sample_name}.txt ${sample_name}
+     vdjtools PlotSpectratypeV VDJtools.${sample_name}.txt ${sample_name}
+
+ ## Diversity Estimation
+     vdjtools PlotQuantileStats VDJtools.${sample_name}.txt ${sample_name}
+
+ done < <(tail -n +2 $metadataToConvert)
